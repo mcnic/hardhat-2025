60e33166d293405d879d751f860e3454497f39ad
max <mcnic@users.noreply.github.com>
2025-03-12 20:21:11 -0700

    fix: libs and Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,9 @@ require('@nomicfoundation/hardhat-toolbox');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.19',
+  defaultNetwork: 'hardhat',
   networks: {
+    hardhat: {},
     sepolia: {
       url: process.env.INFURA_URL || '',
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
